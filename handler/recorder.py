1d86ebdbbd3aa43e2f177bbb9e99a10602b6bf0e
Pathara Norasethasopon <patharanor@gmail.com>
2021-02-20 15:48:47 +0700

    Saved cropped image

--- handler/recorder.py
+++ handler/recorder.py
@@ -1,10 +1,15 @@
 import pandas as pd
-
+import cv2
 class Recorder():
     def __init__(self):
         self.data = {}
 
     def record(self, object_id, class_name, img):
+        # Save image if it is the first look
+        if not (f'{class_name}-{object_id}' in self.data):
+            cv2.imwrite(f'../cropped/{class_name}-{object_id}.jpg', img)
+
+        # Record to dict
         self.data[f'{class_name}-{object_id}'] = [object_id, class_name]
 
     def export(self, fpath):
